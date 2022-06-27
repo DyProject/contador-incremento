d12966a5abca1a213550598e216d2a3f795c80ce
R0ch4011 <pedro.rocha.neni@gmail.com>
2022-06-27 09:34:12 -0300

    Tarefa Pedro

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ void reiniciar(){
 
 /*Tarefa Pedro*/
 void setar_incremento(int inc){
-
+    incremento=inc;
 }
 
 /*Tarefa JAMILLE*/
@@ -32,14 +32,15 @@ void setar_valor_inicial(int valor){
 
 /*Tarefa Pedro*/
 void setar_valor_atual(int valor){
-
+    valor_atual=valor;
 }
+
 
 int main(int argc, char const *argv[])
 {
     char opcao = '+';
     while(opcao != '0') {
-        printf("Opcoes: [+] Incrementar, [-] Decrementar, [*] Reiniciar, [i] Novo incremento, [v] Novo valor atual, [v] Novo valor inicial,[0] Sair: ");
+        printf("Opcoes: [+] Incrementar, [-] Decrementar, [*] Reiniciar, [i] Novo incremento, [c] Novo valor atual, [v] Novo valor inicial,[0] Sair: ");
         scanf("%c", &opcao);
         fflush(stdin);
 
@@ -54,7 +55,19 @@ int main(int argc, char const *argv[])
             printf("Novo valor: ");
             scanf("%d", &valor);
             setar_valor_inicial(valor);
-        }
+        }else if(opcao == 'i') {
+            int valorIncremento;
+            printf("Novo valor de Incremento: ");
+            scanf("%d", &valorIncremento);
+            setar_incremento(valorIncremento);
+         }else if(opcao == 'c') {
+            int valorAtual;
+            printf("Novo valor atual: ");
+            scanf("%d", &valorAtual);
+            setar_valor_atual(valorAtual);
+         }
+
+
 
         printf("valor atual: %d, valor inicial: %d, incremento: %d \n", valor_atual, valor_inicial, incremento);
     }
